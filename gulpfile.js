e504a400e3ad5d6a2d6d3e98ae9ce6ace725ab9a
sachaxod <cashaxod@yandex.ru>
2020-06-02 22:13:02 +0300

    ver 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
-const { src, dest } = require("gulp");
+const { src, dest, task, series } = require("gulp");
+const rm = require("gulp-rm");
+const sass = require('gulp-sass');
 
-function copy() {
-    return src('src/style/main.css').pipe(dest('dist'))
-}
+const files = [
+    'src/**/*.scss',
+    'src/**/*.css',
+]
 
-exports.copy = copy
+sass.compiler = require('node-sass'); //sass компилятор node
+
+task('clean', () => {
+    return src('dist/**/*', { read: false }) //gulp-rm подключение  npm install --save-dev gulp-rm
+        .pipe(rm()) // удаление всех файлов из папки dist
+});
+
+task('copy', () => {
+    return src(files).pipe(dest('dist'))
+        /* копируем файлы с расширением .css, и .scss из папки 
+                                                       src в папку dist*/
+});
+
+task('styles', () => {
+    return src('src/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('dist'));
+    /* сначало установить npm install node-sass gulp-sass --save-dev*/
+});
+
+task("default", series("clean", "styles"));
